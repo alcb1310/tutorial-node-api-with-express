47549e07c8f3dbef1e811ce15dec385c79e4f653
Andrés Court <andres@andrescourt.com>
2022-10-26 17:53:19 -0500

    Added nodemon to the server

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ app.get('/', (req, res) => {
 	res.send('Hello World');
 });
 
-app.listen(port, () => {
-	console.log(`Server started, listening on port ${port}`);
+app.listen(8000, () => {
+	console.log(`Server started, listening on port 8000`);
+	console.log(`Press control-C to stop the server`);
 });
